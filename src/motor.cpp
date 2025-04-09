5c47a4b3fc6cce44c257e587f43ccfb674a05f70
Leader <3251263056@qq.com>
2025-04-09 23:49:33 +0800

    complete motor driver

--- src/motor.cpp
+++ src/motor.cpp
@@ -4,8 +4,7 @@
 #include "unitreeMotor/unitreeMotor.h"
 #include "vmc_quadruped_controller/msg/motor_cmd.hpp"
 #include "vmc_quadruped_controller/msg/motor_data.hpp"
-// #include <thread>
-// #include <chrono>
+#include <memory>
 
 class Motor : public rclcpp::Node
 {
@@ -17,43 +16,29 @@ class Motor : public rclcpp::Node
         publisher = this->create_publisher<vmc_quadruped_controller::msg::MotorData>("motor_data",10);
         subscription = this->create_subscription<vmc_quadruped_controller::msg::MotorCmd>(
             "motor_cmd", 10, std::bind(&Motor::motor_cmd_callback, this, std::placeholders::_1));
-        // while(rclcpp::ok()){
-        //     vmc_quadruped_controller::msg::MotorData data = vmc_quadruped_controller::msg::MotorData();
-        //     data.id = 0;
-        //     data.mode = 1;
-        //     data.tau = 0.21;
-        //     data.vel = 0.11;
-        //     data.pos = 0.178;
-        //     data.temp = 23;
-        //     data.error = 0;
-        //     data.force = 134;
-        //     publisher->publish(data);
-        //     MotorCmd cmd;
-        //     MotorData data1;
-        //     cmd.motorType = MotorType::GO_M8010_6;
-        //     data1.motorType = MotorType::GO_M8010_6;
-        //     cmd.mode = queryMotorMode(MotorType::GO_M8010_6,MotorMode::FOC);
-        //     cmd.id   = 0;
-        //     cmd.kp   = 0.0;
-        //     cmd.kd   = 0.01;
-        //     cmd.q    = 0.0;
-        //     cmd.dq   = -6.28*queryGearRatio(MotorType::GO_M8010_6);
-        //     cmd.tau  = 0.0;
-        //     std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-        // }
     }
     private:
         void motor_cmd_callback(const vmc_quadruped_controller::msg::MotorCmd::SharedPtr msg)const{
-            vmc_quadruped_controller::msg::MotorData data = vmc_quadruped_controller::msg::MotorData();
-            data.id = msg->id;
-            data.mode = msg->mode;
-            data.tau = msg->tau;
-            data.vel = msg->vel;
-            data.pos = msg->pos;
-            data.temp = 23;
-            data.error = 0;
-            data.force = 2331;
-            publisher->publish(data);
+            vmc_quadruped_controller::msg::MotorData motorData = vmc_quadruped_controller::msg::MotorData();
+            MotorCmd cmd;
+            MotorData data;
+            cmd.id = msg->id;
+            cmd.mode = msg->mode;
+            cmd.tau = msg->tau;
+            cmd.kp = msg->kp;
+            cmd.kd = msg->kd;
+            cmd.q = msg->pos;
+            cmd.dq = msg->vel;
+            serial->sendRecv(&cmd,&data);
+            motorData.id = data.motor_id;
+            motorData.mode = data.mode;
+            motorData.tau = data.tau;
+            motorData.vel = data.dq;
+            motorData.pos = data.q;
+            motorData.temp = data.temp;
+            motorData.error = data.merror;
+            motorData.force = data.footForce;
+            publisher->publish(motorData);
         }
 };
 
