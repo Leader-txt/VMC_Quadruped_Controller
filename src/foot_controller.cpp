f8ab392827c0f229824847a8db9dac5f2218d0a7
Leader <3251263056@qq.com>
2025-04-12 04:34:50 +0800

    fix foot controller

--- src/foot_controller.cpp
+++ src/foot_controller.cpp
@@ -7,11 +7,11 @@ class Foot_Controller : public rclcpp::Node{
         serial = new SerialPort("/dev/ttyS7");
         // std::thread leg0(control_leg,0);
         // std::thread leg1(control_leg,1);
-        // std::thread leg2(control_leg,2);
+        std::thread leg2(control_leg,2);
         std::thread leg3(control_leg,3);
         // leg0.join();
         // leg1.join();
-        // leg2.join();
+        leg2.join();
         leg3.join();
     }
 };
@@ -22,12 +22,22 @@ void SendMsg(MotorData* data,MotorCmd* cmd){
     lock.unlock();
 }
 
+float clip(float var,float maxVal){
+    if(var < - maxVal){
+        return -maxVal;
+    }
+    if(var > maxVal){
+        return maxVal;
+    }
+    return var;
+}
+
 void control_leg(uint id){
     // // start count
     // auto start = std::chrono::system_clock::now();
     // int count = 0;
     VMC_Param param;
-    param.kp_x = 100;
+    param.kp_x = 1000;
     param.kd_x = 10;
     param.kp_y = 100;
     param.kd_y = 10;
@@ -79,14 +89,14 @@ void control_leg(uint id){
     kineRes = Kinematic_Solution(OUTER_MOTOR_OFFEST,INNER_MOTOR_OFFEST,0,0);
     target_pos_x = kineRes.pos_x;
     target_pos_y = kineRes.pos_z;
-    target_pos_x = 0;
-    target_pos_y = 0.223;
+    // target_pos_x = 0;
+    // target_pos_y = 0.223;
     // // test target pos out
     // std::cout << target_pos_x << "  " << target_pos_y << std::endl;
     // return;
     while(rclcpp::ok()){
-        angle_alpha = (data_outer.q - outer_motor_offest) / gear_ratio + OUTER_MOTOR_OFFEST;
-        angle_beta = (data_inner.q - inner_motor_offest) / gear_ratio + INNER_MOTOR_OFFEST;
+        angle_alpha = ((data_outer.q - outer_motor_offest) / gear_ratio)*dir_outer + OUTER_MOTOR_OFFEST;
+        angle_beta = ((data_inner.q - inner_motor_offest) / gear_ratio)*dir_inner + INNER_MOTOR_OFFEST;
         vec_alpha = data_outer.dq / gear_ratio * dir_outer;
         vec_beta = data_inner.dq / gear_ratio * dir_inner;
         kineRes = Kinematic_Solution(angle_alpha,angle_beta,vec_alpha,vec_beta);
@@ -94,10 +104,14 @@ void control_leg(uint id){
         jocRes = VMC_Jacobi_Matrix(angle_alpha,angle_beta,vmcRes.force_x,vmcRes.force_z);
         outer_tau = jocRes.tau_alpha / gear_ratio * dir_outer;
         inner_tau = jocRes.tau_beta / gear_ratio * dir_inner;
-        printf("force_x:%.3f force_y:%.3f outer_tau:%.3f inner_tau:%.3f\r\n",vmcRes.force_x,vmcRes.force_z,outer_tau,inner_tau);
+        // outer_tau = clip(outer_tau,0.1);
+        // inner_tau = clip(inner_tau,0.1);
+        // printf("force_x:%.3f force_y:%.3f outer_tau:%.3f inner_tau:%.3f\r\n",vmcRes.force_x,vmcRes.force_z,outer_tau,inner_tau);
+        cmd_outer.tau = outer_tau;
+        cmd_inner.tau = inner_tau;
         SendMsg(&data_outer,&cmd_outer);
         SendMsg(&data_inner,&cmd_inner);
-        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        // std::this_thread::sleep_for(std::chrono::milliseconds(500));
         // // count freq
         // count ++ ;
         // auto time_now = std::chrono::system_clock::now();
@@ -108,6 +122,10 @@ void control_leg(uint id){
         //     count = 0;
         // }
     }
+    cmd_outer.tau = 0;
+    cmd_inner.tau = 0;
+    SendMsg(&data_outer,&cmd_outer);
+    SendMsg(&data_inner,&cmd_inner);
 }
 
 int main(int argc,char* argv[]){
